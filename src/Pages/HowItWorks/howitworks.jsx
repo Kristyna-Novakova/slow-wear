ef16452396e356061dad5d584fb695204506f963
Kristýna Nováková <novakovkri@gmail.com>
2023-05-29 16:10:07 +0200

    Styling HowItWorks Component

---
+++ src/Pages/HowItWorks/howitworks.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css';
+import step1 from './img/step1.png';
+import step2 from './img/step2.png';
+import step3 from './img/step3.png';
+
+export const HowItWorks = () => {
+  return (
+    <section className="section">
+      <h2 className="section-title">Jak to funguje</h2>
+      <div className="image-container">
+        <div className="image">
+          <img src={step1} alt="Obrázek 1" />
+          <h3>Vyber si</h3>
+          <p className="image-caption">
+            Čtyři topy a stejné šaty ve dvou velikostech? Šest párů džín? Jeden
+            kus od všeho? Je to na tobě.
+          </p>
+        </div>
+        <div className="image">
+          <img src={step2} alt="Obrázek 2" />
+          <h3>Nos je opakovaně</h3>
+          <p className="image-caption">
+            Celý další měsíc jsou vybrané kousky jen tvoje. O praní a opravy se
+            nemusíš starat.
+          </p>
+        </div>
+        <div className="image">
+          <img src={step3} alt="Obrázek 3" />
+          <h3>Kup, vrať, opakuj</h3>
+          <p className="image-caption">
+            Pokud se do něčeho zamiluješ, kup si to. Zbytek pošli zpět do data
+            rezervace a opakuj celý proces.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './style.css';
 import { Navbar } from './components/Navbar/navbar';
 import { MainPage } from './Pages/MainPage/mainpage';
+import { HowItWorks } from './Pages/HowItWorks/howitworks';
 
 const App = () => {
   return (
     <div className="container">
       <Navbar />
       <MainPage />
+      <HowItWorks />
       <footer>
         <p>Czechitas, Digitální akademie: Web</p>
       </footer>
