40f3480eb042599c8edca2710d4a6f86e62a8477
Kristýna Nováková <novakovkri@gmail.com>
2023-06-04 15:48:27 +0200

    fixing Categories List

--- src/Components/HamburgerMenu/hamburgerMenu.jsx
+++ src/Components/HamburgerMenu/hamburgerMenu.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export const HamburgerMenu = () => {
+  const [openCategories, setOpenCategories] = useState(false);
+
+  const handleCategoriesList = () => {
+    setOpenCategories(!openCategories);
+  };
+
   return (
     <div className="menu-items">
       <ul>
-        <li>Kategorie</li>
+        <li
+          onClick={handleCategoriesList}
+          className={`categories-menu ${openCategories} ? "" : "close"`}
+        >
+          Kategorie
+        </li>
+        {openCategories && (
+          <div className="categories-list">
+            <ul>
+              <li>Topy</li>
+              <li>Džíny</li>
+              <li>Šaty</li>
+              <li>Bundy</li>
+            </ul>
+          </div>
+        )}
         <li>Jak to funguje</li>
         <li>Přihlášení</li>
         <li>Moje objednávky</li>
