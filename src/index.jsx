d27ed7124092982a2ebee42157b7148c7de31bf5
Kristýna Nováková <novakovkri@gmail.com>
2023-06-05 17:37:38 +0200

    preparing new Components

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './style.css';
 import { Navbar } from './components/Navbar/navbar';
 import { MainPage } from './Pages/MainPage/mainPage';
 import { HowItWorks } from './Pages/HowItWorks/howItWorks';
-import { CategoryView } from './Pages/Categories/categories';
+import { CategoryView } from './Pages/CategoryView/categoryView';
 import { Footer } from './Components/Footer/footer';
 
 const App = () => {
